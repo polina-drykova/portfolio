1678e3203484b00eaaedc696f9c93ba9b3789a16
Polina Drykova <polina.drykova1988@gmail.com>
2019-11-30 19:55:49 +0700

    commented out sweet alert, added notice on the contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,9 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-  redirect_to root_path, notice: 'Message sent successfully'
+      redirect_to root_path(anchor: 'contact'), notice: 'Your message has been sent. I’ll get back within 1-2 days.'
+      # render :js => 'confirmSend()'
+  # redirect_to root_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
